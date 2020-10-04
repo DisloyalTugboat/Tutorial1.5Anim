a3359224563e7cb12da4573206685726998c0b31
DisloyalTugboat <38084022+DisloyalTugboat@users.noreply.github.com>
2020-10-04 12:03:18 -0400

    Done

---
+++ Tutorial1.5Anim/Assets/Scripts/CatController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour
+{
+
+
+    Animator anim;
+
+    void Start()
+
+    {
+
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+
+    void Update()
+
+    {
+
+        if (Input.GetKeyDown(KeyCode.W))
+
+        {
+
+            anim.SetInteger("State", 1);
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+
+        {
+
+            anim.SetInteger("State", 0);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+
+        {
+
+            anim.SetInteger("State", 2);
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.R))
+
+        {
+
+            anim.SetInteger("State", 0);
+
+        }
+
+ 
+
+    }
+}
